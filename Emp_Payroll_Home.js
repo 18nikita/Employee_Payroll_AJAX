2362d2ce39bb3b9d1cf089a321a7c6e3c399a1d1
unknown <rajputnikita5666@gmail.com>
2021-11-30 12:29:53 +0530

    Added:Display all details from json

--- Emp_Payroll_Home.js
+++ Emp_Payroll_Home.js
@@ -3,6 +3,7 @@ window.addEvenListener('DOMContentLoaded',(event) =>{
 });
 
 //Templete features 
+//UC4
 const createInnerHtml = () =>{
 const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                     "<th>Salary</th><th>Start Date</th><th>Actions</th>";
@@ -22,4 +23,32 @@ const innerHtml = `'${headerHtml}
 </tr>
 `;
 document.querySelector('#table-display').innerHtml = innerHtml;
+}
+
+//UC5
+const createInneriital = () =>{
+
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+    "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollList of empPayrollList){
+        innerHtml = `${headerHtml}
+        <tr>
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td> 
+        <td>"${empPayrollData._name}"</td>
+        <td>"${empPayrollData._gender}"</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td> 
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+            <img name="${empPayrollData._id}" onclick="remove(this)" 
+            src="Asset/icons/delete-black-18dp.svg" alt="delete">
+            <img name="${empPayrollData._id}" onclick="update(this)" 
+            src="Asset/icons/create-black-18dp.svg" alt="edit">
+        </td>
+    </tr>`; 
+    }
+    document.querySelector('#table-display').innerHtml = innerHtml;
 }
